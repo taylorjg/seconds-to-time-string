186561aef1975afe9919f47bd46155dd48c1f729
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-12-01 09:13:26 +0000

    Refactor

--- lib.js
+++ lib.js
@@ -1,19 +1,27 @@
+const formatSeconds = (seconds) => {
+  if (seconds === 1) return "1 second";
+  return `${seconds} seconds`;
+};
+
+const formatMinutes = (minutes) => {
+  if (minutes === 1) return "1 minute";
+  return `${minutes} minutes`;
+};
+
 export const secondsToTimeString = (seconds) => {
   if (seconds < 0) return "Don't be silly";
-  if (seconds === 1) return "1 second";
-  if (seconds < 60) return `${seconds} seconds`;
+
+  if (seconds < 60) return formatSeconds(seconds);
 
   const minutes = Math.floor(seconds / 60);
   const remainingSeconds = seconds % 60;
 
   const bits = [];
 
-  bits.push(minutes === 1 ? `1 minute` : `${minutes} minutes`);
+  bits.push(formatMinutes(minutes));
 
   if (remainingSeconds > 0) {
-    bits.push(
-      remainingSeconds === 1 ? `1 second` : `${remainingSeconds} seconds`
-    );
+    bits.push(formatSeconds(remainingSeconds));
   }
 
   return bits.join(` and `);
