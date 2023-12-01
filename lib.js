542f237f799a7819eea0af084d18775dc8bcd6af
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-12-01 08:52:47 +0000

    Add a few more basic test cases

--- lib.js
+++ lib.js
@@ -1,3 +1,5 @@
 export const secondsToTimeString = (seconds) => {
+  if (seconds < 0) return "Don't be silly";
+  if (seconds === 1) return "1 second";
   return `${seconds} seconds`;
 };
--- lib.test.js
+++ lib.test.js
@@ -1,11 +1,10 @@
 import { secondsToTimeString } from "./lib";
 
 describe("secondsToTimeString tests", () => {
-  it("10 seconds", () => {
-    expect(secondsToTimeString(10)).toBe("10 seconds");
-  });
-
   it.each([
+    { seconds: -1, string: "Don't be silly" },
+    { seconds: 0, string: "0 seconds" },
+    { seconds: 1, string: "1 second" },
     { seconds: 10, string: "10 seconds" },
     { seconds: 20, string: "20 seconds" },
   ])("$seconds => $string", ({ seconds, string }) => {
