ab2311478e610cd0d226ba49198448ae76abe87f
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-12-01 09:10:15 +0000

    Add test cases re minutes and seconds

--- lib.js
+++ lib.js
@@ -1,5 +1,20 @@
 export const secondsToTimeString = (seconds) => {
   if (seconds < 0) return "Don't be silly";
   if (seconds === 1) return "1 second";
-  return `${seconds} seconds`;
+  if (seconds < 60) return `${seconds} seconds`;
+
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+
+  const bits = [];
+
+  bits.push(minutes === 1 ? `1 minute` : `${minutes} minutes`);
+
+  if (remainingSeconds > 0) {
+    bits.push(
+      remainingSeconds === 1 ? `1 second` : `${remainingSeconds} seconds`
+    );
+  }
+
+  return bits.join(` and `);
 };
--- lib.test.js
+++ lib.test.js
@@ -5,8 +5,10 @@ describe("secondsToTimeString tests", () => {
     { seconds: -1, string: "Don't be silly" },
     { seconds: 0, string: "0 seconds" },
     { seconds: 1, string: "1 second" },
-    { seconds: 10, string: "10 seconds" },
-    { seconds: 20, string: "20 seconds" },
+    { seconds: 2, string: "2 seconds" },
+    { seconds: 60, string: "1 minute" },
+    { seconds: 120, string: "2 minutes" },
+    { seconds: 121, string: "2 minutes and 1 second" },
   ])("$seconds => $string", ({ seconds, string }) => {
     expect(secondsToTimeString(seconds)).toBe(string);
   });
