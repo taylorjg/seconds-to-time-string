5643f04d97bc866e2afc2bae2bf57a9271df3591
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-12-01 11:11:27 +0000

    Add hours, minutes and seconds test case

--- lib.js
+++ lib.js
@@ -8,21 +8,49 @@ const formatMinutes = (minutes) => {
   return `${minutes} minutes`;
 };
 
+const formatHours = (hours) => {
+  if (hours === 1) return "1 hour";
+  return `${hours} hours`;
+};
+
+const formatBits = (bits) => {
+  if (bits.length > 2) {
+    const firstBits = bits.slice(0, -1);
+    const lastBit = bits.slice(-1)[0];
+    const firstBitsJoined = firstBits.join(", ");
+    return [firstBitsJoined, lastBit].join(" and ");
+  }
+
+  return bits.join(" and ");
+};
+
 export const secondsToTimeString = (seconds) => {
-  if (seconds < 0) return "Don't be silly";
+  if (!Number.isInteger(seconds) || seconds < 0) return "Don't be silly";
+
+  if (seconds === 0) return "";
 
   if (seconds < 60) return formatSeconds(seconds);
 
-  const minutes = Math.floor(seconds / 60);
-  const remainingSeconds = seconds % 60;
-
   const bits = [];
 
-  bits.push(formatMinutes(minutes));
+  if (seconds < 3600) {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
 
-  if (remainingSeconds > 0) {
+    bits.push(formatMinutes(minutes));
+
+    if (remainingSeconds > 0) {
+      bits.push(formatSeconds(remainingSeconds));
+    }
+  } else {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = seconds % 60;
+
+    bits.push(formatHours(hours));
+    bits.push(formatMinutes(minutes));
     bits.push(formatSeconds(remainingSeconds));
   }
 
-  return bits.join(` and `);
+  return formatBits(bits);
 };
--- lib.test.js
+++ lib.test.js
@@ -1,15 +1,32 @@
 import { secondsToTimeString } from "./lib";
 
+const errorResponse = "Don't be silly";
+
 describe("secondsToTimeString tests", () => {
-  it.each([
-    { seconds: -1, string: "Don't be silly" },
-    { seconds: 0, string: "0 seconds" },
-    { seconds: 1, string: "1 second" },
-    { seconds: 2, string: "2 seconds" },
-    { seconds: 60, string: "1 minute" },
-    { seconds: 120, string: "2 minutes" },
-    { seconds: 121, string: "2 minutes and 1 second" },
-  ])("$seconds => $string", ({ seconds, string }) => {
-    expect(secondsToTimeString(seconds)).toBe(string);
+  describe("error scenarios", () => {
+    it.each([
+      { seconds: null, string: errorResponse },
+      { seconds: undefined, string: errorResponse },
+      { seconds: "bogus", string: errorResponse },
+      { seconds: {}, string: errorResponse },
+      { seconds: -1, string: errorResponse },
+      { seconds: 1.5, string: errorResponse },
+      { seconds: 0, string: "" },
+    ])("$seconds => $string", ({ seconds, string }) => {
+      expect(secondsToTimeString(seconds)).toBe(string);
+    });
+  });
+
+  describe("given scenarios", () => {
+    it.each([
+      { seconds: 1, string: "1 second" },
+      { seconds: 2, string: "2 seconds" },
+      { seconds: 60, string: "1 minute" },
+      { seconds: 120, string: "2 minutes" },
+      { seconds: 121, string: "2 minutes and 1 second" },
+      { seconds: 3661, string: "1 hour, 1 minute and 1 second" },
+    ])("$seconds => $string", ({ seconds, string }) => {
+      expect(secondsToTimeString(seconds)).toBe(string);
+    });
   });
 });
